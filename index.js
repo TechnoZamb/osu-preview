e546f2119de6aff07dc944ebacd3491dacb5392b
TechnoZamb <technozamb19@gmail.com>
2023-07-13 19:50:16 +0200

    a

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ import { ProgressBar } from "./progress.js";
 import { parseSkin, asyncLoadImages } from "./skin.js";
 import urlJoin from "./url-join.js";
 
-const mapFolder = 1 ? "songs/1712395 Ashrount - AureoLe ~for Triumph~/" : "songs/1919786 MIMI vs Leah Kate - 10 Things I Hate About Ai no Sukima/";
-const diff = 1 ? "Ashrount - AureoLe ~for Triumph~ (R3m) [FINAL].osu" : "MIMI vs. Leah Kate - 10 Things I Hate About Ai no Sukima (Log Off Now) [ssadasdsa].osu";
+const mapFolder = 0 ? "songs/1712395 Ashrount - AureoLe ~for Triumph~/" : "songs/1919786 MIMI vs Leah Kate - 10 Things I Hate About Ai no Sukima/";
+const diff = 0 ? "Ashrount - AureoLe ~for Triumph~ (R3m) [FINAL].osu" : "MIMI vs. Leah Kate - 10 Things I Hate About Ai no Sukima (Log Off Now) [ssadasdsa].osu";
 
 const BEZIER_SEGMENT_MAX_LENGTH = 10;        // in screen pixels
 var bezierSegmentMaxLengthSqrd;             // in osu pixels, squared
@@ -14,7 +14,7 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 var bufferCanvas, bufferCtx;
 
-const canvasSize = [1600, 1200];
+const canvasSize = [1600, 1000];
 const minMargin = 50;
 var fieldSize = [], margins, bgSize;
 var beatmap, skin;
@@ -24,8 +24,10 @@ var radius;
 var time2 = 0;
 
 var cursorPos;
-var bgdim = 1//0.33;
+var bgdim = 1;
 var drawGrid = true;
+
+var bakedPaths = [];
 
 
 var osuToPixelsX, osuToPixelsY;
@@ -92,13 +94,14 @@ window.onload = async (e) => {
 
     player = window.player = await MusicPlayer.init(mapFolder + beatmap.General.AudioFilename);
     progressBar = new ProgressBar("#progress-bar", player, callback);
-    player.currentTime = 0.015//41.083;
+    player.currentTime = 0.495//41.083;
 }
 
 function callback(time) {
-    //console.log(100 / (performance.now() - time2));
+    //console.log(1000 / (performance.now() - time2));
     time2 = performance.now();
 
+    ctx.globalCompositeOperation = "source-over";
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     // draw background
@@ -134,11 +137,12 @@ function callback(time) {
             ctx.stroke();
         }
     }
-
-    // playfield border
-    ctx.lineWidth = 2;
-    ctx.strokeStyle = "black";
-    ctx.strokeRect(margins[0], margins[1], fieldSize[0], fieldSize[1])
+    else {
+        // playfield border
+        ctx.lineWidth = 2;
+        ctx.strokeStyle = "black";
+        ctx.strokeRect(margins[0], margins[1], fieldSize[0], fieldSize[1]);
+    }
 
     // find first object to paint
     time *= 1000;
@@ -151,25 +155,53 @@ function callback(time) {
 
         if (obj[3] & 2) {      // slider
 
-            var snake = 1;
-
+            var snake;
             bufferCtx.globalCompositeOperation = "source-over";
             bufferCtx.clearRect(0, 0, bufferCanvas.width, bufferCanvas.height);
             bufferCtx.beginPath();
+
             if (time <= obj[2]) {
                 approachQueue.push(obj);
-                bufferCtx.globalAlpha = clamp(0, (time - (obj[2] - preempt)) / fadein, 1);
+                ctx.globalAlpha = clamp(0, (time - (obj[2] - preempt)) / fadein, 1);
                 snake = clamp(0, (time - (obj[2] - preempt)) / fadein, 0.5) * 2;
             }
             else {
-                bufferCtx.globalAlpha = 1;
+                ctx.globalAlpha = 1;
+                snake = 1;
             }
 
             bufferCtx.moveTo(osuToPixelsX(obj[0]) + margins[0], osuToPixelsY(obj[1]) + margins[1]);
+            
             // linear
             if (obj[5][0] == "L") {
+                const desiredlength = obj[7] * snake;
+                var actualLength = 0, prevLength = 0;
+                var prevObj = obj;
+
                 for (let i = 1; i < obj[5].length; i++) {
-                    bufferCtx.lineTo(osuToPixelsX(obj[5][i][0]) + margins[0], osuToPixelsY(obj[5][i][1]) + margins[1]);
+                    const point = obj[5][i];
+                    actualLength += Math.sqrt(Math.pow(point[0] - prevObj[0], 2) + Math.pow(point[1] - prevObj[1], 2));
+
+                    if (actualLength > desiredlength) {
+                        const ratio = (desiredlength - prevLength) / (actualLength - prevLength);
+                        bufferCtx.lineTo(osuToPixelsX(prevObj[0] + (point[0] - prevObj[0]) * ratio) + margins[0], osuToPixelsY(prevObj[1] + (point[1] - prevObj[1]) * ratio) + margins[1]);
+                        break;
+                    }
+                    else {
+                        bufferCtx.lineTo(osuToPixelsX(point[0]) + margins[0], osuToPixelsY(point[1]) + margins[1]);
+                    }
+
+                    prevObj = point;
+                    prevLength = actualLength;
+                }
+
+                if (actualLength < desiredlength) {
+                    const point = obj[5].at(-1);
+                    prevObj = obj[5].at(-2) ?? obj;
+
+                    prevLength = actualLength - Math.sqrt(Math.pow(point[0] - prevObj[0], 2) + Math.pow(point[1] - prevObj[1], 2));
+                    const ratio = (desiredlength - prevLength) / (actualLength - prevLength);
+                    bufferCtx.lineTo(osuToPixelsX(prevObj[0] + (point[0] - prevObj[0]) * ratio) + margins[0], osuToPixelsY(prevObj[1] + (point[1] - prevObj[1]) * ratio) + margins[1]);
                 }
             }
             // perfect circle
@@ -194,15 +226,15 @@ function callback(time) {
                 const det = determinant([[a[0], a[1], 1], [b[0], b[1], 1], [c[0], c[1], 1]]);
 
                 const arclength = radius * (det < 0 ? mod(anglea - anglec, 2 * Math.PI) : mod(anglec - anglea, 2 * Math.PI));
-                var xincr, yincr;
-                if (arclength > obj[7]) {
-                    anglec = anglea + obj[7] / radius * (det > 0 ? 1 : -1);
+                const desiredlength = obj[7] * snake;
+                var xincr = null, yincr = null;
+                if (arclength > desiredlength) {
+                    anglec = anglea + desiredlength / radius * (det > 0 ? 1 : -1);
                 }
                 else {
                     const slope = 1 / Math.tan(anglec);
-                    xincr = Math.sqrt(Math.pow(obj[7] - arclength, 2) / (1 + slope * slope)) * (anglec < 0 ? -1 : 1);
+                    xincr = Math.sqrt(Math.pow(desiredlength - arclength, 2) / (1 + slope * slope)) * (anglec < 0 ? -1 : 1);
                     yincr = xincr * slope * (anglec < Math.PI ? -1 : 1);
-                    console.log(anglec, slope)
                 }
 
                 bufferCtx.arc(osuToPixelsX(x) + margins[0], osuToPixelsY(y) + margins[1], osuToPixelsX(radius), anglea, anglec, det < 0);
@@ -213,21 +245,68 @@ function callback(time) {
             else if (obj[5][0] == "B" || (obj[5][0] == "P" && obj[5].length > 3)) {
                 const controlPoints = [[obj[0], obj[1]], ...obj[5].slice(1)];
                 var pointsBuffer = [controlPoints[0]];
-                var totlength = [0]
-                for (let i = 1; i < controlPoints.length; i++) {
-                    if (controlPoints[i][0] == controlPoints[i - 1][0] && controlPoints[i][1] == controlPoints[i - 1][1]) {
-                        const points = bezierPoints(pointsBuffer, 0, 1, [], totlength);
-                        for (let point of points)
-                            bufferCtx.lineTo(osuToPixelsX(point[0]) + margins[0], osuToPixelsY(point[1]) + margins[1]);
+
+                const desiredlength = obj[7] * snake;
+                var actualLength = 0, prevLength = 0;
+                var prevObj = obj;
+
+                for (let i = 1; i < controlPoints.length + 1; i++) {
+                    if (i == controlPoints.length || controlPoints[i][0] == controlPoints[i - 1][0] && controlPoints[i][1] == controlPoints[i - 1][1]) {
+                        
+                        if (pointsBuffer.length == 2) {
+                            const point = pointsBuffer.at(-1);
+                            actualLength += Math.sqrt(Math.pow(point[0] - prevObj[0], 2) + Math.pow(point[1] - prevObj[1], 2));
+
+                            if (actualLength > desiredlength) {
+                                const ratio = (desiredlength - prevLength) / (actualLength - prevLength);
+                                bufferCtx.lineTo(osuToPixelsX(prevObj[0] + (point[0] - prevObj[0]) * ratio) + margins[0], osuToPixelsY(prevObj[1] + (point[1] - prevObj[1]) * ratio) + margins[1]);
+                                break; // out
+                            }
+                            else {
+                                bufferCtx.lineTo(osuToPixelsX(point[0]) + margins[0], osuToPixelsY(point[1]) + margins[1]);
+                            }
+
+                            prevObj = point;
+                            prevLength = actualLength;                            
+                        }
+                        else {
+                            var points, lengths;
+
+                            if (!bakedPaths[index]) {
+                                bakedPaths[index] = [];
+                            }
+                            if (!bakedPaths[index][i]) {
+                                [points, lengths] = bakedPaths[index][i] = bezierPoints(pointsBuffer);
+                            }
+                            else {
+                                [points, lengths] = bakedPaths[index][i];
+                            }
+                            
+                            let j;
+                            for (j = 0; j < points.length; j++) {
+                                if (actualLength + lengths[j] > desiredlength) {
+                                    break; // out
+                                }
+                                else {
+                                    bufferCtx.lineTo(osuToPixelsX(points[j][0]) + margins[0], osuToPixelsY(points[j][1]) + margins[1]);                                    
+                                }
+                            }
+
+                            if (j != points.length) {
+                                break; // out
+                            }
+                            else {
+                                prevObj = points.at(-1);
+                                actualLength += lengths.at(-1);
+                                prevLength = actualLength;
+                            }
+                        }
+
                         pointsBuffer = [];
                     }
+
                     pointsBuffer.push(controlPoints[i]);
                 }
-
-                const points = bezierPoints(pointsBuffer, 0, 1, [], totlength);
-                for (let point of points)
-                    bufferCtx.lineTo(osuToPixelsX(point[0]) + margins[0], osuToPixelsY(point[1]) + margins[1]);
-
             }
 
             const diameter = radius * 2 / 512 * fieldSize[0] * 0.8;
@@ -236,7 +315,7 @@ function callback(time) {
             bufferCtx.globalAlpha = 1
 
             // slider border
-            bufferCtx.lineWidth = diameter + 30;
+            bufferCtx.lineWidth = diameter * 1.15;
             bufferCtx.strokeStyle = "#ffffffa0";
             bufferCtx.stroke();
             bufferCtx.lineWidth = diameter;
@@ -246,16 +325,17 @@ function callback(time) {
 
             // slider gradient
             bufferCtx.globalCompositeOperation = "source-over";
-            for (let i = diameter; i > 0; i -= 1000) {
-                bufferCtx.lineWidth = diameter * i / diameter;
-                const perc = (1 - i / diameter) / 255 * 43;
-                //bufferCtx.globalAlpha = 1 - (i / diameter)
-                bufferCtx.strokeStyle = `rgb(0,0,0,0.65)`;
+            bufferCtx.lineWidth = diameter;
+            bufferCtx.strokeStyle = `rgb(0,0,0,0.64)`;
+            bufferCtx.stroke();
+            
+            bufferCtx.globalAlpha = 0.1;
+            for (let divs = 20, i = divs - 1; i > 0; i--) {
+                bufferCtx.lineWidth = diameter * i / divs;
+                bufferCtx.strokeStyle = `rgb(43,43,43)`;
                 bufferCtx.stroke();
             }
             
-
-            ctx.globalAlpha = 1;
             ctx.drawImage(bufferCanvas, 0, 0);
         }
         if (obj[3] & 1 || obj[3] & 2) {
@@ -315,26 +395,33 @@ function callback(time) {
     ctx.globalAlpha = 1;
 
     ctx.fillStyle = "rgb(255,0,0,0.5)";
+    ctx.globalCompositeOperation = "destination-out"
+    var curSize = 100;
     if (cursorPos) {
-        //ctx.fillRect(cursorPos[0] - 20 - canvas.offsetLeft, cursorPos[1] - 20 - canvas.offsetTop, 40, 40)
+        //ctx.fillRect(cursorPos[0] - curSize / 2 - canvas.offsetLeft, cursorPos[1] - curSize / 2 - canvas.offsetTop, curSize, curSize)
     }
 }
 
-const bezierPoints = (points, start, end, arr = [], totlength = []) => {
-    const startP = bezierAt(points, start);
-    const endP = bezierAt(points, end);
-    const length = Math.pow(startP[0] - endP[0], 2) + Math.pow(startP[1] - endP[1], 2);
+const bezierPoints = (points, start = 0, end = 1) => {
+    const arr = [], lengths = [0];
 
-    if (length > bezierSegmentMaxLengthSqrd) {
-        bezierPoints(points, start, (start + end) / 2, arr, totlength);
-        bezierPoints(points, (start + end) / 2, end, arr, totlength);
-        return arr;
-    }
-    else {
-        arr.push(endP);
-        totlength[0] = totlength[0] + Math.sqrt(length);
-        return arr;
-    }
+    const _bezierPoints = (start, end) => {
+        const startP = bezierAt(points, start);
+        const endP = bezierAt(points, end);
+        const length = Math.pow(startP[0] - endP[0], 2) + Math.pow(startP[1] - endP[1], 2);
+
+        if (length > bezierSegmentMaxLengthSqrd) {
+            _bezierPoints(start, (start + end) / 2);
+            _bezierPoints((start + end) / 2, end);
+        }
+        else {
+            arr.push(endP);
+            lengths.push(lengths.at(-1) + Math.sqrt(length));
+        }
+    };
+    _bezierPoints(start, end);
+
+    return [arr, lengths];
 }
 
 const bezierAt = (points, t) => {
